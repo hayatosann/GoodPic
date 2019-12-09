681f5ba0fc4ad848bbdc890fdb70ec212aaddcab
SasanoHayato <sasano.hayato@gmail.com>
2019-12-09 22:28:30 +0900

    Execute relation between user&post and like

--- app/Like.php
+++ app/Like.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     //
+    Public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
+  Public function post()
+  {
+    return $this->belongsTo('App\Post');
+  }
 }
