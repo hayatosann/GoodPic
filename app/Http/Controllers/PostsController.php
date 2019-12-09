42acfed20fd9656da83a972588db31318b2055c2
SasanoHayato <sasano.hayato@gmail.com>
2019-12-09 21:10:11 +0900

    write down new&store action

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use Auth;
+use Validator;
+
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -11,4 +15,28 @@ class PostsController extends Controller
       return view('post/index');
     }
     //
+    public function new()
+    {
+        return view('post/new');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all() , ['caption' => 'required|max:255', 'photo' => 'required']);
+
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
+
+        $post = new Post;
+        $post->caption = $request->caption;
+        $post->user_id = Auth::user()->id;
+
+        $post->save();
+        
+        $request->photo->storeAs('public/post_images', $post->id . '.jpg');
+        
+        return redirect('/');
+    }
 }
